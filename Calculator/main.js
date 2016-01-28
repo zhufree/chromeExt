3183ea8dafe674863b23e7e6d3d6c28b24c6d34f
zhufree <zhufree2013@gmail.com>
2016-01-28 18:09:55 +0800

    去了键盘，改直接输入，chrome不支持eval

--- Calculator/main.js
+++ Calculator/main.js
@@ -1,19 +1,29 @@
-console.log('here');
-var inp = function(){
-	// var $this = $(this);
-	var num = this.text();
-	$('pre#show-express').text($('pre#show-express').val() + num);
-}
+// console.log('here');
+// var inp = function(){
+// 	// var $this = $(this);
+// 	var num = this.text();
+// 	$('pre#show-express').text($('pre#show-express').val() + num);
+// }
 
-var btns = document.getElementsByName('button');
-console.log(btns);
-console.log(btns.length);
-for(var i = 0; i < btns.length; i ++){
-	console.log(btns[i].innerText);
-	btns[i].onclick = function(){
-		console.log(this);
-	// var $this = $(this);
-		var num = this.text();
-		$('pre#show-express').text($('pre#show-express').val() + num);
-	}
-}
+// var btns = document.getElementsByName('button');
+// console.log(btns);
+// console.log(btns.length);
+// for(var i = 0; i < btns.length; i ++){
+// 	console.log(btns[i].innerText);
+// 	btns[i].onclick = function(){
+// 		console.log(this);
+// 	// var $this = $(this);
+// 		var num = this.text();
+// 		$('pre#show-express').text($('pre#show-express').val() + num);
+// 	}
+// }
+$(document).ready(function(){
+	$('#input-btns').hide();
+	$('#show-express').change(function() {
+		var express = $(this).val();
+		console.log(express);
+		var result = eval(express);
+		console.log(result);
+		$('#show-result').text(result);
+	});
+});
